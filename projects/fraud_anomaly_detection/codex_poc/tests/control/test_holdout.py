fb44f3e6330ac157fa6e0ed88729dd43000c8864
SoulEvill <lwd891214@gmail.com>
2026-06-13 14:05:15 -0700

    fraud control: two-state holdout split

---
+++ projects/fraud_anomaly_detection/codex_poc/tests/control/test_holdout.py
@@ -0,0 +1,10 @@
+from projects.fraud_anomaly_detection.codex_poc.control import holdout
+from projects.fraud_anomaly_detection.codex_poc.control.config import ControlConfig
+
+
+def test_split_partitions_users_by_cutoff(tiny_store):
+    split = holdout.two_state_split(tiny_store, ControlConfig(holdout_days=30))
+
+    assert set(split.state_a_users) == {"u1", "u2", "u5"}
+    assert set(split.holdout_users) == {"u3", "u4"}
+    assert split.cutoff < split.newest
